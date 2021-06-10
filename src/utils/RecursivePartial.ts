747a5123b08d22676dcaa8eee77c7a250b6a7d3a
Iain <iain@zora.co>
2021-06-10 13:43:31 -0400

    adding comments

--- src/utils/RecursivePartial.ts
+++ src/utils/RecursivePartial.ts
@@ -1,3 +1,7 @@
+/**
+ * Allows the type to be partial recursively. Useful for merged configuration objects
+ * From: https://stackoverflow.com/questions/41980195/recursive-partialt-in-typescript
+ */
 export type RecursivePartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]
     ? RecursivePartial<U>[]
