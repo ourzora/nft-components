1b0931288e9b71cc2183bda72621172686448896
Dain Blodorn Kim <dain@db13.us>
2021-09-21 16:47:03 -0700

    added useDefaultStyles flag to style theming object in mediaContext

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -63,6 +63,7 @@ function renderSVG(svg: string) {
 
 export const Style = {
   theme: ThemeOptions,
+  useDefaultStyles: <boolean>true,
   styles: {
     auctionHouseList: (_: ThemeOptionsType) => css`
       display: flex;
--- src/context/useMediaContext.ts
+++ src/context/useMediaContext.ts
@@ -7,6 +7,8 @@ import { camelCase } from '../utils/camelCase'
 export function useMediaContext() {
   const mediaContext = useContext(MediaContext);
 
+  console.log(mediaContext)
+
   const getStyles = (
     themeKey: keyof ThemeType["styles"],
     flags: any = {}
@@ -40,7 +42,7 @@ export function useMediaContext() {
     }
 
     return {
-      className: `zora-${themeKey} ${css(styles)} ${getUtilitySelectors(flags)}`,
+      className: `zora-${themeKey} ${css(mediaContext.style.useDefaultStyles ? styles : false)} ${getUtilitySelectors(flags)}`,
     };
   };
 
