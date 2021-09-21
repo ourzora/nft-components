bf27ab59f0976f7b8ac526d6cfe9204dc24e1c5e
Dain Blodorn Kim <dain@db13.us>
2021-09-20 17:46:07 -0700

    proposal: add selector for current state of auction on MediaThumbnailWrapper. similar to what we have been using in the auction houses

--- src/nft-preview/MediaThumbnailWrapper.tsx
+++ src/nft-preview/MediaThumbnailWrapper.tsx
@@ -3,6 +3,43 @@ import { useContext } from "react";
 import { NFTDataContext } from "../context/NFTDataContext";
 import { useMediaContext } from "../context/useMediaContext";
 
+export function useAuctionStatusSelector(pricingData: any) {
+  console.log(pricingData)
+  return `${pricingData?.auctionType} ${pricingData?.status}`
+  /*
+  
+  This is logic used in the create auction house template - would be nice to add similar selectors to the cardOuter div.
+  
+  if (auctions && auctions.length) {
+    const auction = auctions[0]
+    // CHECK: Ended Date < Current Date
+    const currentDate = new Date()
+    const endedDate = auction.expiresAt !== null && new Date(auction.expiresAt)
+    
+    const unclaimed =
+      endedDate !== false && endedDate.getTime() < currentDate.getTime()
+      && auction.winner === null
+    
+    // CHECK: Has Auction Data
+    const hasAuction = auction.bidEvents.length > 0 && auction.winner === null
+
+    if (auction.bidEvents.length === 0) {
+      return 'listed'
+    } else if (hasAuction && !unclaimed) {
+      return 'auction-live'
+    } else if (hasAuction && unclaimed) {
+      return 'unclaimed'
+    } else if (auction.winner !== null && !unclaimed) {
+      return 'ended'
+    } 
+  } else if (auctions === undefined) {
+    return 'not-listed'
+  } else {
+    return 'not-listed'
+  }
+  */
+}
+
 export const MediaThumbnailWrapper = ({
   children,
   onClick,
@@ -21,7 +58,7 @@ export const MediaThumbnailWrapper = ({
 
   return (
     <div
-      {...getStyles("cardOuter", { hasClickEvent: !!onClick, auctionStatus })}
+      {...getStyles("cardOuter", { hasClickEvent: !!onClick, auctionStatus }, useAuctionStatusSelector(nft?.data?.pricing))}
     >
       {(href || onClick) && (
         <LinkComponent {...getStyles("cardLink")} href={href} onClick={onClick}>
