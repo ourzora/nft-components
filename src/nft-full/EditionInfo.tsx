5a577a163f5a364802c8f18c453d8e94a3ff4d3e
Neokry <33191293+neokry@users.noreply.github.com>
2022-03-24 11:30:11 -0700

    Adds Editions (#120)
    * Adds editions to pricing component
    
    * Adds edition info component
    
    * Adds hooks back to package
    
    * Removes log

---
+++ src/nft-full/EditionInfo.tsx
@@ -0,0 +1,51 @@
+import { Fragment, useContext } from "react";
+import { useMediaContext } from "../context/useMediaContext";
+import { NFTDataContext } from "../context/NFTDataContext";
+import type { StyleProps } from "../utils/StyleTypes";
+import { InfoContainer, InfoContainerProps } from "./InfoContainer";
+import { useMemo } from "react";
+import type { EditionLike } from "@zoralabs/nft-hooks/dist/backends/NFTInterface";
+
+export const EditionInfo = ({ className }: StyleProps) => {
+  const { data } = useContext(NFTDataContext);
+  const { getStyles, getString } = useMediaContext();
+
+  const edition = useMemo(
+    () =>
+      data?.markets?.find(
+        (market) => market.type === "Edition" && market.status === "active"
+      ),
+    [data?.markets]
+  ) as undefined | EditionLike;
+
+  const EditionInfoWrapper = ({
+    children,
+    ...containerArgs
+  }: InfoContainerProps) => (
+    <InfoContainer {...containerArgs} className={className}>
+      {children}
+    </InfoContainer>
+  );
+
+  if (!edition) {
+    return <Fragment />;
+  }
+
+  return (
+    <EditionInfoWrapper titleString="EDITION_PRICE">
+      <div {...getStyles("pricingAmount")}>
+        {edition.amount.prettyAmount && (
+          <Fragment>
+            {" "}
+            {edition.amount.prettyAmount} {edition.amount.symbol}
+          </Fragment>
+        )}
+        <div>
+          <div {...getStyles("fullInfoSpacer")} />
+          <div {...getStyles("fullLabel")}>{getString("NFTS_COLLECTED")}</div>
+          {`${edition.totalSupply} / ${edition.editionSize}`}
+        </div>
+      </div>
+    </EditionInfoWrapper>
+  );
+};
