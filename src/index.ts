1a059d6d4ef5b3daff334e393e31780356522f1e
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-13 08:49:15 -0300

    I remove Zora components.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import { Networks } from "@zoralabs/nft-hooks";
-
-import { MediaConfiguration } from "./context/MediaConfiguration";
-import * as MediaRenderers from "./content-components";
-import * as RendererConfigTypes from "./content-components/RendererConfig";
-import { NFTDataProvider } from "./context/NFTDataProvider";
-import { MediaObject } from "./components/MediaObject";
-import { NFTDataContext } from "./context/NFTDataContext";
-import { NFTPreview, PreviewComponents } from "./nft-preview";
-import { NFTFullPage, FullComponents } from "./nft-full";
 import AudioPlayer from "./audio-player/AudioPlayer";
 import {
   DispatchPlayerContext,
@@ -24,24 +14,5 @@ export {
   DispatchPlayerContext,
   PlayerContext,
   playerReducer,
-  playerInitialState, 
-  // Constant list of all networks
-  Networks,
-  // Contextual wrapper component for media configuration
-  MediaConfiguration,
-  // Media rendering component
-  // Preview thumbnail renderer
-  NFTPreview,
-  PreviewComponents,
-  // Full Page renderer
-  NFTFullPage,
-  FullComponents,
-  // Generic data wrapper
-  NFTDataProvider,
-  // Data context for fetching NFT info with custom components
-  NFTDataContext,
-  MediaObject,
-  // Renderers and default array for configuration
-  MediaRenderers,
-  RendererConfigTypes,
+  playerInitialState
 };
